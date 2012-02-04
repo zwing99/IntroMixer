8b4fd967ce2c4096e74c40f467ecf13bc9c1080b
Oler, Zac <zac.oler@gmail.com>
2012-02-04 03:28:39 +0000

    Add

---
+++ UpwardsIntroductionSoundMixer/DataClasses/ShuffleExtension.cs
@@ -0,0 +1,33 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Class1.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace UpwardsIntroductionSoundMixer.DataClasses
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public static class ShuffleExtension
+    {
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            Random rng = new Random();
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+    }
+}
